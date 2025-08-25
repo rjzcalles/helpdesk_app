08575f3d42af7ec77decb62eed7e8570219d0927
Your Name <rodrigojosezaldanacalles@gmail.com>
2025-08-25 09:28:24 +0200

    cambio en el archivo config

---
+++ server/config/config.js
@@ -0,0 +1,28 @@
+// server/config/config.js
+
+// Cargar las variables de entorno del archivo .env
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || 'helpdesk_db',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME_TEST || 'helpdesk_db_test',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  }
+};
